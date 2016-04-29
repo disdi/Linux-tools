0414e136c674af3264fa312715b251d4799f47c9
Aaron Franke <arnfranke@yahoo.com>
2016-04-28 22:24:50 -0500

    Major formatting update, added ubuntu-sso-client-qt

--- programs-to-install.sh
+++ programs-to-install.sh
@@ -15,23 +15,39 @@ sudo apt full-upgrade -y
 
 sudo apt install -y vlc
 
-sudo apt install -y screenfetch gksu pv
+sudo apt install -y screenfetch 
+
+sudo apt install -y gksu 
+
+sudo apt install -y pv
 
 sudo apt install -y notepadqq
 
 sudo apt install -y grub-customizer
 
-sudo apt install -y ffmpeg python-gpgme
+sudo apt install -y ffmpeg 
+
+sudo apt install -y python-gpgme
 
 sudo apt install -y libtcmalloc-minimal4
 
-sudo apt install -y mono-complete mono-vbnc libopenal1
+sudo apt install -y mono-complete mono-vbnc 
 
-sudo apt install -y gparted thunar youtube-dl gedit
+sudo apt install -y libopenal1
+
+sudo apt install -y gparted 
+
+sudo apt install -y thunar 
+
+sudo apt install -y youtube-dl 
+
+sudo apt install -y gedit
 
 sudo apt install -y cowsay fortune-mod
 
-sudo apt install -y openjdk-8-jre psensor
+sudo apt install -y openjdk-8-jre 
+
+sudo apt install -y psensor
 
 sudo apt install -y libdvd-pkg
 
@@ -39,9 +55,13 @@ sudo apt install -y gimp
 
 sudo apt install -y software-center
 
+sudo apt install -y ubuntu-sso-client-qt
+
 sudo apt install -y libreoffice
 
-sudo apt install -y wine playonlinux
+sudo apt install -y wine 
+
+sudo apt install -y playonlinux
 
 sudo update-ca-certificates -f
 
